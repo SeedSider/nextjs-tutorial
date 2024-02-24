37c5f2810868f233df278117917a76deee9c4a0f
SeedSider <sidiqusman13@gmail.com>
2024-02-24 08:20:51 +0700

    Done tutorial next.js

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,7 +11,6 @@ import {
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
-import { error } from 'console';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
@@ -79,7 +78,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   {error}
                 </p>
               ))}
-          </div>
+            </div>
           </div>
         </div>
 
@@ -122,7 +121,18 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
+          <div id="status-error" aria-live='polite' aria-atomic="true">
+              {state.errors?.status &&
+                state.errors.status.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+          </div>
         </fieldset>
+        <div id="any-error" aria-live='polite' aria-atomic="true">
+          <p className="mt-2 text-sm text-red-500">{state.message}</p>
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
